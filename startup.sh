948196c0545c84a23c5d6b8f0a48883832a870c0
hamsringdaoLangthasa <45279614+hamsringdaoLangthasa@users.noreply.github.com>
2024-06-26 14:16:24 +0000

    add updated files

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
+ echo "Running kubeadmin init"
+ 
  kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+
+ echo "Running kubectl apply"
  kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
  
